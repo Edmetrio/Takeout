a63fddc017b60a8dac9d73b645ba630e4767d096
Edmetrio <edmetrio06@gmail.com>
2021-08-13 14:23:19 +0200

    configurar modal artigo

--- app/Models/Models/Artigo.php
+++ app/Models/Models/Artigo.php
@@ -4,8 +4,16 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Artigo extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'artigo';
+    protected $fillable = ['nome','estado'];
 }
