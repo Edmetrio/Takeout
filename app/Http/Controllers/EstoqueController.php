e4aa2c460f820c43483fb43547afc265e1c87824
Edmetrio <edmetrio06@gmail.com>
2021-08-14 21:06:26 +0200

    adicionar dados no estoque

---
+++ app/Http/Controllers/EstoqueController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Models\Artigo;
+use App\Models\Models\Estoque;
+use Illuminate\Http\Request;
+
+class EstoqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $artigo = Artigo::latest()->get();
+        return view('createEstoque', compact('artigo'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /* return $request->all(); */
+        $request->validate([
+            'artigo_id' => 'required',
+            'users_id' => 'required',
+            'quantidade' => 'required',
+            'quantidade_minima' => 'required',
+        ]);
+        $estoque = Estoque::create($request->all());
+        if ($estoque) {
+            $request->session()->flash('status', 'Item Estoque adicionado com Sucesso!');
+            return redirect('estoque/create');
+        }
+        $request->session()->flash('status', 'Erro ao Adicionar!');
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
