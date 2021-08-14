777fba92643901b9dafdbd084bac5debb28455f6
Edmetrio <edmetrio06@gmail.com>
2021-08-14 21:57:11 +0200

    conigurar dados do aumentaEstoque

--- app/Models/Models/AumentaEstoque.php
+++ app/Models/Models/AumentaEstoque.php
@@ -4,8 +4,17 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class AumentaEstoque extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'users_id';
+
+    protected $table = 'aumentaestoque';
+    protected $fillable = ['users_id','artigo_id','quantidade','quantidade_minima','preco_compra'];
 }
