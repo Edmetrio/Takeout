03aad3c96afd928986e309d4cefd403fb3cb9853
Edmetrio <edmetrio06@gmail.com>
2021-08-15 14:58:08 +0200

    relacionamento com artigos e users

--- app/Models/Models/Estoque.php
+++ app/Models/Models/Estoque.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
@@ -17,4 +18,15 @@ class Estoque extends Model
 
     protected $table = 'estoque';
     protected $fillable = ['users_id','artigo_id','quantidade','quantidade_minima','preco_compra'];
+
+    public function artigos()
+    {
+        return $this->hasOne(Artigo::class, 'id', 'artigo_id');
+    }
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
 }
