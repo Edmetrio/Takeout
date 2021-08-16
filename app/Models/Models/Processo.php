df6f3b9128d47ba08f1ce04abcd91486ed15da21
Edmetrio <edmetrio06@gmail.com>
2021-08-16 14:41:38 +0200

    relacionamento artigo e produto

--- app/Models/Models/Processo.php
+++ app/Models/Models/Processo.php
@@ -4,8 +4,27 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Processo extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'processo';
+    protected $fillable = ['artigo_id','produto_id','quantidade'];
+
+    public function artigos()
+    {
+        return $this->hasMany(Artigo::class);
+    }
+
+    public function produtos()
+    {
+        return $this->belongsTo(Produto::class);
+    }
 }
