75f9366915521eb269bd2006cf4ff1a7f0c09b2e
Edmetrio <edmetrio06@gmail.com>
2021-08-16 15:21:26 +0200

    listagem no index do processo

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -16,7 +16,9 @@ class ProcessoController extends Controller
      */
     public function index()
     {
-        //
+        $processo = Processo::with(['produtos','artigos'])->get();
+        dd($processo);
+        return view('processo');
     }
 
     /**
