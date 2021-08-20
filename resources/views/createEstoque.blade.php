9b0be1b138f55d8b5d59927884b450c1d3daaf13
Edmetrio <edmetrio06@gmail.com>
2021-08-20 23:50:21 +0200

    ajustes do blade do estoque

--- resources/views/createEstoque.blade.php
+++ resources/views/createEstoque.blade.php
@@ -15,7 +15,7 @@
                     </ol>
                 </nav>
 
-                <h1 class="m-0">@if(isset($produto))Alterar @else Cadastro @endif da Produto</h1>
+                <h1 class="m-0">Cadastro do Estoque</h1>
             </div>
         </div>
         @if(session('status'))
@@ -28,7 +28,7 @@
             <div class="card card-form">
                 <div class="row no-gutters">
                     <div class="col-lg-4 card-body">
-                        <p><strong class="headings-color">Categoria</strong></p>
+                        <p><strong class="headings-color">Estoque</strong></p>
                         <p class="text-muted">As Categorias Existentes no sistema são: Hambúrgueres, Racheis, Dose de Batatas e de Frango
                         </p>
                     </div>
@@ -40,41 +40,36 @@
                         @endif
                     </div>
                     <div class="col-lg-8 card-form__body card-body">
-                        @if(isset($estoque))
-                        <form method="POST" action="{{url("estoque/$estoque->id")}}">
-                            @method('PUT')
-                            @else
-                            <form method="POST" action="{{url('estoque')}}">
-                                @endif
-                                @csrf
-                                <div class="was-validated">
-                                    <div class="form-row">
-                                        <div class="col-12 col-md-6 mb-12">
-                                            <label for="validationSample01">Nome</label>
-                                            <select name="artigo_id" id="artigo_id">
-                                               
-                                                @foreach($artigo as $a)
-                                                <option value="{{$a->id}}">{{$a->nome}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="col-12 col-md-6 mb-3">
-                                            <label for="validationSample01">Quantidade</label>
-                                            <input type="text" class="form-control" name="quantidade" placeholder="12" required="">
-                                            <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
-                                        </div>
-                                        <div class="col-12 col-md-6 mb-3">
-                                            <label for="validationSample01">Quantidade Mínima</label>
-                                            <input type="text" class="form-control" name="quantidade_minima" placeholder="12" required="">
-                                        </div>
-                                        <div class="col-12 col-md-6 mb-3">
-                                            <label for="validationSample01">Preço de Compra</label>
-                                            <input type="text" class="form-control" name="preco_compra" placeholder="12" required="">
-                                        </div>
+                        <form method="POST" action="{{url('estoque')}}">
+                            @csrf
+                            <div class="was-validated">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <label for="validationSample01">Nome do Artigo</label>
+                                        <select name="artigo_id" id="artigo_id" class="form-control">
+                                            <option value="">Seleccione o artigo</option>
+                                            @foreach($artigo as $a)
+                                            <option value="{{$a->id}}">{{$a->nome}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-12 col-md-6 mb-3">
+                                        <label for="validationSample01">Quantidade</label>
+                                        <input type="text" class="form-control" name="quantidade" placeholder="12" required="">
+                                        <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                                    </div>
+                                    <div class="col-12 col-md-6 mb-3">
+                                        <label for="validationSample01">Quantidade Mínima</label>
+                                        <input type="text" class="form-control" name="quantidade_minima" placeholder="12" required="">
+                                    </div>
+                                    <div class="col-12 col-md-6 mb-3">
+                                        <label for="validationSample01">Preço de Compra</label>
+                                        <input type="text" class="form-control" name="preco_compra" placeholder="12" required="">
                                     </div>
                                 </div>
-                                <button class="btn btn-primary" type="submit">@if(isset($produto))Alterar @else Submeter @endif</button>
-                            </form>
+                            </div>
+                            <button class="btn btn-primary" type="submit">@if(isset($produto))Alterar @else Submeter @endif</button>
+                        </form>
                     </div>
                 </div>
             </div>
