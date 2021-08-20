0f5443e79c630a89a02db0fd25fa9813000cf7de
Edmetrio <edmetrio06@gmail.com>
2021-08-20 23:47:05 +0200

    listar atraves do processo

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -17,6 +17,7 @@ class ProcessoController extends Controller
     public function index()
     {
         $processo = Processo::with(['produtos','artigos'])->get();
+        /* $processo = Produto::with('processos')->get(); */
         /* dd($processo); */
         return view('processo', compact('processo'));
     }
