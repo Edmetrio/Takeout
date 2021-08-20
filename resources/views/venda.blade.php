dc8b21e33b94ea7f0a15a89adb1d07068217042d
Edmetrio <edmetrio06@gmail.com>
2021-08-20 23:52:40 +0200

    inserção e config. do botão início e finaliza

--- resources/views/venda.blade.php
+++ resources/views/venda.blade.php
@@ -6,16 +6,36 @@
     <div class="mdk-drawer-layout__content page">
 
         <div class="container-fluid page__heading-container">
-            <div class="page__heading">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="#"><i class="material-icons icon-20pt">home</i></a></li>
-                        <li class="breadcrumb-item">UI Components</li>
-                        <li class="breadcrumb-item active" aria-current="page">Tables</li>
-                    </ol>
-                </nav>
-
-                <h1 class="m-0">Lista dos produtos</h1>
+            <div class="page__heading d-flex align-items-center">
+                <div class="flex">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="#"><i class="material-icons icon-20pt">home</i></a></li>
+                            <li class="breadcrumb-item">UI Venda</li>
+                            <li class="breadcrumb-item active">Venda</li>
+                        </ol>
+                    </nav>
+                    <h1 class="m-0">Lista dos Produto</h1>
+                </div>
+                @if(session('status'))
+                <div class="alert alert-success" role="alert" style="text-align: center; font-weight: bold;">
+                    <p class="status">{{session('status')}}</p>
+                </div>
+                @endif
+                @if(!isset($venda->users_id) && !isset($venda->pagamento_id))
+                    <form action="{{url('iniciar')}}" method="post">
+                        @csrf
+                        <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                        <button class="btn btn-primary" type="submit">Iniciar</button>
+                    </form>
+                @elseif(isset($venda->users_id) && !isset($venda->pagamento_id))
+                <form action="{{url('iniciar')}}" method="post">
+                        @csrf
+                        <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                        <button class="btn btn-primary" type="submit">Finalizar</button>
+                    </form>
+                @endif
+                <a href="{{url('itemvenda/create')}}" class="btn btn-success ml-3">Adicionar</a>
             </div>
         </div>
         @if(session('status'))
@@ -24,212 +44,46 @@
         </div>
         @endif
         <div class="container-fluid page__container">
-
-
             <div class="card card-form">
                 <div class="row no-gutters">
-                    <div class="container-fluid page__heading-container">
-                        <div class="page__heading d-flex align-items-center">
-                            <div class="flex">
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb mb-0">
-                                        <li class="breadcrumb-item"><a href="#"><i class="material-icons icon-20pt">home</i></a></li>
-                                        <li class="breadcrumb-item active">Estoque</li>
-                                    </ol>
-                                </nav>
-                                <h1 class="m-0">Estoque</h1>
-                            </div>
-                            <a href="{{url('estoque/create')}}" class="btn btn-success ml-3">Adicionar</a>
-                        </div>
+                    <div class="col-lg-4 card-body">
+                        <p><strong class="headings-color">Lista das vendas</strong></p>
+                        <p class="text-muted">Encontra listada todas vendas existentes na sua loja virtual</p>
                     </div>
-                    <div class="container-fluid page__container">
-                            <div class="card">
+                    <div class="col-lg-8 card-form__body">
 
-                                <div class="table-responsive">
+                        <div class="table-responsive border-bottom" data-toggle="lists" data-lists-values='["js-lists-values-employee-name"]'>
 
-                                    <div class="m-3">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <select name="#"
-                                                        class="form-control">
-                                                    <option value="-1">All</option>
-                                                    <option value="1">Hats</option>
-                                                    <option value="2">Coats</option>
-                                                    <option value="3">Jeans</option>
-                                                    <option value="4">T-Shirt</option>
-                                                    <option value="5">Other</option>
-                                                </select>
-                                            </div>
-                                            <div class="col-md-8">
-                                                <div class="search-form search-form--light">
-                                                    <input type="text"
-                                                           class="form-control search"
-                                                           placeholder="Search">
-                                                    <button class="btn"
-                                                            type="button"
-                                                            role="button"><i class="material-icons">search</i></button>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <table class="table mb-0 thead-border-top-0 table-striped">
-                                        <thead>
-                                            <tr>
-
-                                                <th style="width: 18px;">
-                                                    <div class="custom-control custom-checkbox">
-                                                        <input type="checkbox"
-                                                               class="custom-control-input js-toggle-check-all"
-                                                               data-target="#products"
-                                                               id="customCheckAll">
-                                                        <label class="custom-control-label"
-                                                               for="customCheckAll"><span class="text-hide">Toggle all</span></label>
-                                                    </div>
-                                                </th>
-
-                                                <th style="width: 30px;"
-                                                    class="text-center">#ID</th>
-                                                <th>Product</th>
-                                                <th class="text-center">Stock</th>
-                                                <th class="">Category</th>
-                                                <th class="text-right">Price</th>
-                                                <th style="width: 100px; text-align: right;">
-                                                    <div class="dropdown pull-right">
-                                                        <a href="#"
-                                                           data-toggle="dropdown"
-                                                           class="dropdown-toggle">Bulk</a>
-                                                        <div class="dropdown-menu dropdown-menu-right">
-                                                            <a href="javascript:void(0)"
-                                                               class="dropdown-item"><i class="material-icons icon-18pt mr-1">work</i> Set Price</a>
-                                                            <a href="javascript:void(0)"
-                                                               class="dropdown-item"><i class="material-icons icon-18pt mr-1">archive</i> Archive</a>
-                                                        </div>
-                                                    </div>
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody class="list"
-                                               id="products">
-
-                                            <tr>
-
-                                                <td class="text-center">
-                                                    <div class="custom-control custom-checkbox">
-                                                        <input type="checkbox"
-                                                               class="custom-control-input js-check-selected-row"
-                                                               id="customCheck1_1">
-                                                        <label class="custom-control-label"
-                                                               for="customCheck1_1"><span class="text-hide">Check</span></label>
-                                                    </div>
-                                                </td>
-                                                @foreach($estoque as $e)
-                                                <td>
-                                                    <div class="badge badge-soft-dark">{{$e->users->name}}</div>
-                                                </td>
-                                                <td>
-                                                    <img src="assets/images/Amora.jpg"
-                                                         alt="product"
-                                                         style="width:35px"
-                                                         class="rounded mr-2">
-                                                    <a href="#">{{$e->artigos->nome}}</a>
-                                                </td>
-                                                <td style="width: 120px;"
-                                                    class="text-center">
-                                                    {{$e->quantidade}} itens</td>
-                                                <td style="width:200px">
-
-                                                    <i class="material-icons icon-18pt text-muted mr-1">list</i> <a href="#">Coats</a>
-
-                                                </td>
-
-                                                <td class="text-right">{{$e->preco_compra}}</td>
-                                                <td class="text-right"><a href="#"
-                                                       class="btn btn-sm btn-primary">EDIT</td>
-                                            </tr>
-                                             @endforeach       
-                                            <tr>
-
-                                                <td class="text-center">
-                                                    <div class="custom-control custom-checkbox">
-                                                        <input type="checkbox"
-                                                               class="custom-control-input js-check-selected-row"
-                                                               id="customCheck1_2">
-                                                        <label class="custom-control-label"
-                                                               for="customCheck1_2"><span class="text-hide">Check</span></label>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <div class="badge badge-soft-dark">#102</div>
-                                                </td>
-                                                <td>
-                                                    <img src="assets/images/avatars/lobostudio-hamburg-64728.jpg"
-                                                         alt="product"
-                                                         style="width:35px"
-                                                         class="rounded mr-2">
-                                                    <a href="#">Navy Hat with Stripes</a>
-                                                </td>
-                                                <td style="width: 120px;"
-                                                    class="text-center">
-                                                    10 items</td>
-                                                <td style="width:200px">
-
-                                                    <i class="material-icons icon-18pt text-muted mr-1">list</i> <a href="#">Hats</a>
-
-                                                </td>
-
-                                                <td class="text-right">$54</td>
-                                                <td class="text-right"><a href="#"
-                                                       class="btn btn-sm btn-primary">EDIT</td>
-                                            </tr>
-
-                                            <tr>
-
-                                                <td class="text-center">
-                                                    <div class="custom-control custom-checkbox">
-                                                        <input type="checkbox"
-                                                               class="custom-control-input js-check-selected-row"
-                                                               id="customCheck1_3">
-                                                        <label class="custom-control-label"
-                                                               for="customCheck1_3"><span class="text-hide">Check</span></label>
-                                                    </div>
-                                                </td>
-
-                                                <td>
-                                                    <div class="badge badge-soft-dark">#103</div>
-                                                </td>
-                                                <td>
-                                                    <img src="assets/images/avatars/nina-strehl-140734.jpg"
-                                                         alt="product"
-                                                         style="width:35px"
-                                                         class="rounded mr-2">
-                                                    <a href="#">Bleu Snow Coat</a>
-                                                </td>
-                                                <td style="width: 120px;"
-                                                    class="text-center">
-                                                    4 items</td>
-                                                <td style="width:200px">
-
-                                                    <i class="material-icons icon-18pt text-muted mr-1">list</i> <a href="#">Coats</a>
-
-                                                </td>
-
-                                                <td class="text-right">$22</td>
-                                                <td class="text-right"><a href="#"
-                                                       class="btn btn-sm btn-primary">EDIT</td>
-                                            
-                                        </tbody>
-                                    </table>
-                                </div>
-
-                                <div class="card-body text-right">
-                                    15 <span class="text-muted">of 25</span> <a href="#"
-                                       class="text-muted-light"><i class="material-icons ml-1">arrow_forward</i></a>
-                                </div>
-
+                            <div class="search-form search-form--light m-3">
+                                <input type="text" class="form-control search" placeholder="Search">
+                                <button class="btn" type="button"><i class="material-icons">search</i></button>
                             </div>
+
+                            <table class="table mb-0 thead-border-top-0">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 200px;">Nome do Produto</th>
+                                        <th style="width: 37px;">Quantidade</th>
+                                        <th style="width: 20px;">Preço</th>
+                                        <th style="width: 24px;">Subtotal</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="list" id="staff02">
+
+                                    <p></p>
+                                    @foreach($item as $i)
+                                    <tr>
+                                        <td><strong><span class="js-lists-values-employee-name">{{$i->produtos->nome}}</span></strong></td>
+                                        <td><span class="js-lists-values-employee-name">{{$i->quantidade}}</span></td>
+                                        <td><span class="js-lists-values-employee-name">{{$i->produtos->preco}}</span></td>
+                                        <td><span>{{$i->subtotal}}</span></td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
+                        <p><strong class="headings-color" style="float: right; color: cadetblue; font-size: 25px; text-align: left;">{{$total}},00</strong></p>
+                    </div>
                 </div>
             </div>
 
