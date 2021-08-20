ad4fb4b3f85924464d48e91ba56d304394c03630
Edmetrio <edmetrio06@gmail.com>
2021-08-20 23:48:38 +0200

    Model de venda

--- app/Models/Models/Venda.php
+++ app/Models/Models/Venda.php
@@ -4,8 +4,22 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Venda extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'venda';
+    protected $fillable = ['users_id','pagamento_id','valor_total'];
+
+    public function itemvendas()
+    {
+        return $this->hasMany(itemvenda::class, 'venda_id');
+    }
 }
