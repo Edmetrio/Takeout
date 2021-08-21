8b580fae777c9ceea4cda0e15583766403b36f0e
Edmetrio <edmetrio06@gmail.com>
2021-08-21 20:11:43 +0200

    model do histórico

--- app/Models/Models/Historico.php
+++ app/Models/Models/Historico.php
@@ -4,8 +4,17 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Historico extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'historico';
+    protected $fillable = ['venda_id','pagamento_id','valor_total'];
 }
