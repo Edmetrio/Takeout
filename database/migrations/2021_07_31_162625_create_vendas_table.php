9bc676eaf8153f5cad9fce41a61e050dab7e73cf
Edmetrio <edmetrio06@gmail.com>
2021-08-21 20:13:03 +0200

    inserção do primary key por default

--- database/migrations/2021_07_31_162625_create_vendas_table.php
+++ database/migrations/2021_07_31_162625_create_vendas_table.php
@@ -14,7 +14,7 @@ class CreateVendasTable extends Migration
     public function up()
     {
         Schema::create('venda', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default('1fed24c2-0cd9-4c3c-80f6-d873601fa103');
             $table->uuid('users_id');
             $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->uuid('pagamento_id');
