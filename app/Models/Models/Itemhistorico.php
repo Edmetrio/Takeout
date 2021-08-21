65b3627b6b97053d42ec897431785bd046b014ff
Edmetrio <edmetrio06@gmail.com>
2021-08-21 20:12:08 +0200

    model do item histórico

--- app/Models/Models/Itemhistorico.php
+++ app/Models/Models/Itemhistorico.php
@@ -4,8 +4,17 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Itemhistorico extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'itemhistorico';
+    protected $fillable = ['historico_id','produto_id','quantidade'];
 }
