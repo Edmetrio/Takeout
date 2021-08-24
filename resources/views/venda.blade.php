f43ef52eac96644d1a56e3a90a4a74a01800364d
Edmetrio <edmetrio06@gmail.com>
2021-08-24 09:14:10 +0200

    area de cadastro na venda

--- resources/views/venda.blade.php
+++ resources/views/venda.blade.php
@@ -17,19 +17,56 @@
                     </nav>
                     <h1 class="m-0">Lista dos Produto</h1>
                 </div>
-                @if(session('status'))
-                <div class="alert alert-success" role="alert" style="text-align: center; font-weight: bold;">
-                    <p class="status">{{session('status')}}</p>
-                </div>
-                @endif
                 @if(!isset($venda->users_id) && !isset($venda->pagamento_id))
-                    <form action="{{url('iniciar')}}" method="post">
-                        @csrf
-                        <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
-                        <button class="btn btn-primary" type="submit">Iniciar</button>
-                    </form>
+                <form action="{{url('iniciar')}}" method="post">
+                    @csrf
+                    <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                    <button class="btn btn-primary" type="submit">Iniciar</button>
+                </form>
+                @elseif(!isset($venda->valor_total) && isset($venda->pagamento_id))
+                <form action="{{url('iniciar')}}" method="post">
+                    @csrf
+                    <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                    <button class="btn btn-primary" type="submit">Iniciar</button>
+                </form>
                 @endif
                 <a href="{{url('itemvenda/create')}}" class="btn btn-success ml-3">Adicionar</a>
+            </div>
+        </div>
+        <div class="container-fluid page__container">
+
+            <div class="card card-form">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 card-body">
+                        <p><strong class="headings-color">Produto</strong></p>
+                        <p class="text-muted">Os Produtos Existentes no sistema são: Hambúrgueres, Racheis, Dose de Batatas e de Frango
+                        </p>
+                    </div>
+                    <div class="col-lg-8 card-form__body card-body">
+                            <form method="POST" action="{{url('itemvenda')}}">
+                            @csrf
+                            <div class="was-validated">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <label for="validationSample01">Nome do Produto</label>
+                                        <select name="produto_id" id="produto_id" class="form-control">
+                                        <option value="">Seleccione o Produto</option>
+                                            @foreach($produto as $p)
+                                            <option value="{{$p->id}}">{{$p->nome}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-12 col-md-6 mb-3">
+                                        <label for="validationSample01">Quantidade</label>
+                                        <input type="text" class="form-control" name="quantidade" placeholder="1" value="" required="">
+                                        <input type="text" class="form-control" name="venda_id" placeholder="1" value="{{$venda->id}}" hidden required="">
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="btn btn-primary" type="submit">@if(isset($produto))Alterar @else Adicionar @endif</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
         @if(session('status'))
@@ -81,14 +118,14 @@
                 </div>
             </div>
             @if(isset($venda->users_id) && !isset($venda->pagamento_id))
-                <form action="{{url('item')}}" method="post">
-                        @csrf
-                        <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
-                        <input type="text" class="form-control" name="valor_total" value="{{$total}}" placeholder="12" required="">
-                        <input type="text" class="form-control" name="pagamento_id"  value="f9587ab4-c1f1-4c4f-bb73-cad838831a6d" placeholder="12" required="">
-                        <button class="btn btn-primary" type="submit" style="float: right;">Finalizar</button>
-                    </form>
-                @endif
+            <form action="{{url('item')}}" method="post">
+                @csrf
+                <input type="text" class="form-control" name="users_id" hidden value="69b81095-f5f0-42a9-a68b-e6dbbceef0c2" placeholder="12" required="">
+                <input type="text" class="form-control" name="valor_total" value="{{$total}}" placeholder="12" required="">
+                <input type="text" class="form-control" name="pagamento_id" value="f9587ab4-c1f1-4c4f-bb73-cad838831a6d" placeholder="12" required="">
+                <button class="btn btn-primary" type="submit" style="float: right;">Finalizar</button>
+            </form>
+            @endif
         </div>
 
     </div>
