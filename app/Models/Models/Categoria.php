33c25776b10679888d084df3a875f0d4c23b0f2c
Edmetrio <edmetrio06@gmail.com>
2021-08-27 14:04:14 +0200

    relacionamento categoria

--- app/Models/Models/Categoria.php
+++ app/Models/Models/Categoria.php
@@ -5,7 +5,6 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
-
 class Categoria extends Model
 {
     use HasFactory, Uuid;
