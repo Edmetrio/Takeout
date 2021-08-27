1830da995ee2a572bc2bad28d1099111bc88713f
Edmetrio <edmetrio06@gmail.com>
2021-08-27 14:04:39 +0200

    model conta a pagar

--- app/Models/Models/Contapagar.php
+++ app/Models/Models/Contapagar.php
@@ -2,10 +2,33 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 class Contapagar extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'contapagar';
+    protected $fillable = ['users_id','estado_id','tipo_id','valor','descricao','estado_id'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
+
+    public function tipos()
+    {
+        return $this->hasOne(Tipo::class, 'id', 'tipo_id');
+    }
+
+    public function estados()
+    {
+        return $this->hasOne(Estado::class, 'id', 'estado_id');
+    }
 }
