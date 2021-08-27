b1b571caae27269df37a0851dbf5bace102cccf3
Edmetrio <edmetrio06@gmail.com>
2021-08-27 14:05:01 +0200

    model estado

--- app/Models/Models/Estado.php
+++ app/Models/Models/Estado.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Estado extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'estado';
+    protected $fillable = ['nome'];
+
+    public function tipos()
+    {
+        return $this->hasMany(Tipo::class, 'contapagar');
+    }
 }
