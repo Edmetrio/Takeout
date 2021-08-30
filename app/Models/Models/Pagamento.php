7c1512d9bee554655556a3a594c796a4f7c746f8
Edmetrio <edmetrio06@gmail.com>
2021-08-30 11:05:23 +0200

    dados do pagamento

--- app/Models/Models/Pagamento.php
+++ app/Models/Models/Pagamento.php
@@ -4,8 +4,17 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Pagamento extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'pagamento';
+    protected $fillable = ['nome'];
 }
