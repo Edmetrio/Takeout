245f1be30117492d9eeaa5f29094dd3ef4d06d56
Edmetrio <edmetrio06@gmail.com>
2021-09-01 14:43:14 +0200

    relacionamento user

--- app/Models/Models/Perfil.php
+++ app/Models/Models/Perfil.php
@@ -2,10 +2,25 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
 class Perfil extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'perfil';
+    protected $fillable = ['users_id','contacto','foto','endereco'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
 }
