ebc5ae551bcca25601e190209bb063c3bbef8a02
Edmetrio <edmetrio06@gmail.com>
2021-09-02 22:23:33 +0200

    exclusão de pagamento_id

--- database/migrations/2021_07_31_163624_create_historicos_table.php
+++ database/migrations/2021_07_31_163624_create_historicos_table.php
@@ -17,8 +17,6 @@ class CreateHistoricosTable extends Migration
             $table->uuid('id')->primary();
             $table->uuid('users_id');
             $table->foreign('users_id')->references('id')->on('users');
-            $table->uuid('pagamento_id');
-            $table->foreign('pagamento_id')->references('id')->on('pagamento');
             $table->decimal('valor_total',20,2);
             $table->timestamps();
         });
