9724ace4e747763d9f17deddb125a07ab335d22e
Edmetrio <edmetrio06@gmail.com>
2021-09-14 15:11:11 +0200

    model quebra de estoque

---
+++ app/Models/Models/Quebra.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Quebra extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+
+    protected $table = 'quebra';
+    protected $fillable = ['users_id','artigo_id','quantidade','descricao'];
+
+    public function users()
+    {
+        return $this->hasOne(User::class, 'id','users_id');
+    }
+
+    public function artigos()
+    {
+        return $this->hasOne(Artigo::class, 'id','artigo_id');
+    }
+}
