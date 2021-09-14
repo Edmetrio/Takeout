8e33cbdafb054e55d628dd3fda7682b39287997e
Edmetrio <edmetrio06@gmail.com>
2021-09-14 15:11:47 +0200

    tabela nota

---
+++ database/migrations/2021_09_14_080804_create_notas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nota', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('users_id');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->string('estado')->default('on');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nota');
+    }
+}
